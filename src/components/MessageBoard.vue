5b32b31b0cc96b15d41f5f502e8ddd5ba5baed84
ssuto <ss.time2531@gmail.com>
2020-08-23 17:27:39 +0900

    Update to fetch message data fromfirestore

--- src/components/MessageBoard.vue
+++ src/components/MessageBoard.vue
@@ -1,6 +1,7 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 <template>
   <v-container>
-    <message-card />
+    <message-card v-for="(message, i) in messages" :key="i" :message="message" />
   </v-container>
 </template>
 
@@ -8,11 +9,24 @@
 import Vue from "vue";
 import { Component } from "vue-property-decorator";
 import MessageCard from "@/components/MessageCard.vue";
+import { Unsubscribe } from "firebase";
+import { db } from "@/plugins/firebase";
 
 @Component({
   components: {
     MessageCard,
   },
 })
-export default class MessageBoard extends Vue {}
+export default class MessageBoard extends Vue {
+  detacher: Unsubscribe | undefined = undefined;
+  messages: Array<Record<string, any>> = [];
+
+  created() {
+    this.detacher = db.collection("message-board").onSnapshot((snapshot) => {
+      this.messages = snapshot.docs.map((doc) => {
+        return Object.assign(doc.data());
+      });
+    });
+  }
+}
 </script>
--- src/components/MessageCard.vue
+++ src/components/MessageCard.vue
@@ -2,11 +2,13 @@
   <v-row justify="center">
     <v-col cols="12" md="6">
       <v-card color="#FFFFFF">
-        <v-card-title class="pa-3">Title</v-card-title>
-        <v-card-subtitle class="pa-3">Name, Date</v-card-subtitle>
-        <v-card-text class="pa-3 font-weight-medium">Content</v-card-text>
+        <v-card-title class="pa-3">{{ message.title }}</v-card-title>
+        <v-card-subtitle
+          class="pa-3"
+        >{{ message.name }} {{ message.timeCreated && message.timeCreated.toDate().toLocaleString() }}</v-card-subtitle>
+        <v-card-text class="pa-3 font-weight-medium">{{ message.message }}</v-card-text>
         <div class="pa-3" align="end">
-          <v-btn>コメントする</v-btn>
+          <v-btn>Comment</v-btn>
         </div>
 
         <comment-card />
@@ -17,7 +19,7 @@
 
 <script lang="ts">
 import Vue from "vue";
-import { Component } from "vue-property-decorator";
+import { Component, Prop } from "vue-property-decorator";
 import CommentCard from "@/components/CommentCard.vue";
 
 @Component({
@@ -25,5 +27,7 @@ import CommentCard from "@/components/CommentCard.vue";
     CommentCard,
   },
 })
-export default class MessageCard extends Vue {}
+export default class MessageCard extends Vue {
+  @Prop(Object) readonly message: object | undefined;
+}
 </script>
